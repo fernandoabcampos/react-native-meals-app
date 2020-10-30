a051150f971832b72e84cb40cf77511ce2996538
Fernando Barbeiro <fernandoabcampos@gmail.com>
2020-10-30 09:11:13 +0100

    Adding and loading a new font to the app

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "dosis-light": require("./assets/fonts/Dosis-Light.ttf"),
+    "dosis-regular": require("./assets/fonts/Dosis-Regular.ttf"),
+    "dosis-bold": require("./assets/fonts/Dosis-Medium.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.font}>
+        OPEN UP App.js to start working on your app!
+      </Text>
     </View>
   );
 }
@@ -14,8 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  font: {
+    fontFamily: "dosis-regular",
+    fontSize: 18
   },
 });
