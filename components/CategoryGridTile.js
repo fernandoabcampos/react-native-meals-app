48337ba781dbfc1ec5c2623c270972b2d98cb897
Fernando Barbeiro <fernandoabcampos@gmail.com>
2020-11-08 15:55:23 +0100

    Component styling.

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,23 +1,58 @@
 import React from "react";
-import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableNativeFeedback,
+} from "react-native";
 
 const CategoryGridTile = (props) => {
+  let TouchableComponent = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableComponent = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity
-      onPress={props.onSelect}
-      style={styles.gridItem}
-    >
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
+    <View style={styles.gridItem}>
+      <TouchableComponent style={{ flex: 1 }} onPress={props.onSelect}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text style={styles.title} numberOfLines={2}>
+            {props.title}
+          </Text>
+        </View>
+      </TouchableComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
   gridItem: {
     flex: 1,
     margin: 15,
     height: 150,
-  }, 
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  title: {
+    fontFamily: "dosis-bold",
+    fontSize: 22,
+    textAlign: "right",
+  },
 });
 
 export default CategoryGridTile;
