77c867ff6334c88232559e3779c41a038dcdf17d
Fernando Barbeiro <fernandoabcampos@gmail.com>
2020-11-15 17:36:14 +0100

    Adding Header Buttons

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -2,6 +2,9 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
@@ -23,6 +26,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Fav!");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
